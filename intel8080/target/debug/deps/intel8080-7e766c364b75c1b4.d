73d2b5243b416a8b995e21111eee13af31f06080
Akram Fedora <akramahmadgb@gmail.com>
2021-09-06 01:04:42 +0100

    Initial commit

---
+++ intel8080/target/debug/deps/intel8080-7e766c364b75c1b4.d
@@ -0,0 +1,5 @@
+/home/akram/Source/rust/intel8080/target/debug/deps/intel8080-7e766c364b75c1b4: src/main.rs
+
+/home/akram/Source/rust/intel8080/target/debug/deps/intel8080-7e766c364b75c1b4.d: src/main.rs
+
+src/main.rs:
---
+++ intel8080/target/debug/intel8080.d
@@ -0,0 +1,1 @@
+/home/akram/Source/rust/intel8080/target/debug/intel8080: /home/akram/Source/rust/intel8080/src/main.rs
